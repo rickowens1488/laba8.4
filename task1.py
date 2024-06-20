654d10562a6ce62bc254f217e306cf86a75de2a5
rickowens1488 <164238996+rickowens1488@users.noreply.github.com>
2024-06-21 01:14:09 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+from random import choice
+
+answers = ['Норм.', 'Лучше всех :)', 'Ну так', 'Отличненько!', 'Ничего, жить буду']
+
+def how_are_you():
+    answer = choice(answers)
+    return answer
+
+print(how_are_you())
